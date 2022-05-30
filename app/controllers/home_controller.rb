f43ff6d6b028122fa2870a107c5750f99e214e05
guillermo-delprado <galfonso.delprado@gmail.com>
2022-05-29 22:26:46 -0500

    Showing the data in index.html with the request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
-
+require 'uri'
+require 'net/http'
 class HomeController < ApplicationController
   def index
+    
+    url = "https://api.openweathermap.org/data/2.5/weather?lat=25.77748&lon=-100.18331&units=metric&appid=9ec69f380e58f9bea96de06552313721"
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    @data = JSON.parse(res.body)
   end
 end
